b486515cf8c662eff6ca297aedeaaa060e168c79
B33 <my@email.com>
2020-01-30 11:54:00 -0600

    Add functionality to caculate button

--- PhotoOrder/Form1.Designer.cs
+++ PhotoOrder/Form1.Designer.cs
@@ -28,36 +28,37 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.comboBox1 = new System.Windows.Forms.ComboBox();
-      this.textBox1 = new System.Windows.Forms.TextBox();
-      this.button1 = new System.Windows.Forms.Button();
+      this.cmbSizes = new System.Windows.Forms.ComboBox();
+      this.txtPrice = new System.Windows.Forms.TextBox();
+      this.buttCalc = new System.Windows.Forms.Button();
       this.label1 = new System.Windows.Forms.Label();
       this.label2 = new System.Windows.Forms.Label();
       this.SuspendLayout();
       // 
-      // comboBox1
+      // cmbSizes
       // 
-      this.comboBox1.FormattingEnabled = true;
-      this.comboBox1.Location = new System.Drawing.Point(185, 28);
-      this.comboBox1.Name = "comboBox1";
-      this.comboBox1.Size = new System.Drawing.Size(121, 33);
-      this.comboBox1.TabIndex = 0;
+      this.cmbSizes.FormattingEnabled = true;
+      this.cmbSizes.Location = new System.Drawing.Point(185, 28);
+      this.cmbSizes.Name = "cmbSizes";
+      this.cmbSizes.Size = new System.Drawing.Size(121, 33);
+      this.cmbSizes.TabIndex = 0;
       // 
-      // textBox1
+      // txtPrice
       // 
-      this.textBox1.Location = new System.Drawing.Point(185, 84);
-      this.textBox1.Name = "textBox1";
-      this.textBox1.Size = new System.Drawing.Size(121, 31);
-      this.textBox1.TabIndex = 1;
+      this.txtPrice.Location = new System.Drawing.Point(185, 84);
+      this.txtPrice.Name = "txtPrice";
+      this.txtPrice.Size = new System.Drawing.Size(121, 31);
+      this.txtPrice.TabIndex = 1;
       // 
-      // button1
+      // buttCalc
       // 
-      this.button1.Location = new System.Drawing.Point(91, 162);
-      this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(160, 36);
-      this.button1.TabIndex = 2;
-      this.button1.Text = "Caculate Price";
-      this.button1.UseVisualStyleBackColor = true;
+      this.buttCalc.Location = new System.Drawing.Point(91, 162);
+      this.buttCalc.Name = "buttCalc";
+      this.buttCalc.Size = new System.Drawing.Size(160, 36);
+      this.buttCalc.TabIndex = 2;
+      this.buttCalc.Text = "Caculate Price";
+      this.buttCalc.UseVisualStyleBackColor = true;
+      this.buttCalc.Click += new System.EventHandler(this.buttCalc_Click);
       // 
       // label1
       // 
@@ -84,11 +85,12 @@
       this.ClientSize = new System.Drawing.Size(340, 224);
       this.Controls.Add(this.label2);
       this.Controls.Add(this.label1);
-      this.Controls.Add(this.button1);
-      this.Controls.Add(this.textBox1);
-      this.Controls.Add(this.comboBox1);
+      this.Controls.Add(this.buttCalc);
+      this.Controls.Add(this.txtPrice);
+      this.Controls.Add(this.cmbSizes);
       this.Name = "Form1";
       this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -96,9 +98,9 @@
 
     #endregion
 
-    private System.Windows.Forms.ComboBox comboBox1;
-    private System.Windows.Forms.TextBox textBox1;
-    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.ComboBox cmbSizes;
+    private System.Windows.Forms.TextBox txtPrice;
+    private System.Windows.Forms.Button buttCalc;
     private System.Windows.Forms.Label label1;
     private System.Windows.Forms.Label label2;
   }
--- PhotoOrder/Form1.cs
+++ PhotoOrder/Form1.cs
@@ -16,5 +16,37 @@ namespace PhotoOrder
     {
       InitializeComponent();
     }
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      cmbSizes.Items.Add("Small");
+      cmbSizes.Items.Add("Medium");
+      cmbSizes.Items.Add("Large");
+
+      cmbSizes.SelectedIndex = 0;
+    }
+
+    private void buttCalc_Click(object sender, EventArgs e)
+    {
+      String itemSize = cmbSizes.SelectedItem.ToString();
+
+      double price;
+
+      switch (itemSize){
+        case "Small":
+          price = 0.20;
+          break;
+        case "Medium":
+          price = 0.40;
+          break;
+        case "Large":
+          price = 0.60;
+          break;
+        default:
+          MessageBox.Show("Please pick a size.", "Error");
+          return;
+
+          txtPrice.Text = price.ToString("c");
+      } 
+    }
   }
 }
